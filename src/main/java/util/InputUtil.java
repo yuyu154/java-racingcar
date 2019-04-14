874459b3ad9f500a548aa84ec57f7f3606241b12
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-04-14 20:26:22 +0900

    [추가] InputUtil 사용자 입력 기능 추가

---
+++ src/main/java/util/InputUtil.java
@@ -0,0 +1,32 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputUtil {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String COMMA = ",";
+    
+    public static int getInt() {
+        try {
+            return Integer.parseInt(getString());
+        } catch (RuntimeException e) {
+            return getInt();
+        }
+    }
+
+    public static List<String> getStrSplitWithComma() {
+        try {
+            return Arrays.asList(getString().split(COMMA));
+        } catch (RuntimeException e) {
+            return getStrSplitWithComma();
+        }
+    }
+
+
+    private static String getString() {
+        return SCANNER.nextLine();
+    }
+}
